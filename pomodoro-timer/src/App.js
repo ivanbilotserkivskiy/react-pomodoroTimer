8608cfc9899ccbaba304dd62afec177647821eef
Ivan Bilotserkivskiy <ivanbilotserkivskiy@gmail.com>
2023-03-05 19:36:13 +0200

    start react app

---
+++ pomodoro-timer/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import WorkPage from './components/WorkPage';
+
+function App() {
+  return (
+    
+    <WorkPage/>
+
+  );
+}
+
+export default App;
---
+++ pomodoro-timer/src/components/WorkPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function WorkPage (props) {
+
+    return (
+        <div className='workPage-div'>
+        <h1 className='workPage-title'>Work Time</h1>
+        <p className='workPage-timer'>15:00</p>
+        <button className='workPage-btn'>Start</button>
+        </div>
+    )
+
+}
+
